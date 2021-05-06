7478a58e5f1e1cfb0efc5e9484f09bb7d5ab6ab7
arjita <arjita.sri.28@gmail.com>
2021-05-06 12:11:05 +0530

    1. Testing basic functionality of typescript.

---
+++ sandbox.ts
@@ -0,0 +1,12 @@
+//basic typescript
+
+
+const character = 'mario';
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  console.log(input);
+});
