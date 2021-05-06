551e52b95d0f3538196dd49d5bccbd94d1e655ca
arjitaSrivastava0 <arjita.sri.28@gmail.com>
2021-05-06 13:25:54 +0530

    DOM_and_tycasting

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,10 @@
-// classes
-class Invoice {
-  client: string;
-  details: string;
-  amount: number;
-
-  constructor(c: string, d: string, a: number){
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
-
-  format() {
-    return `${this.client} owes £${this.amount} for ${this.details}`;
-  }
+const anchor = document.querySelector('a')!;
+if(anchor) {
+  console.log(anchor.href);
 }
+console.log(anchor.href);
 
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-
-let invoices: Invoice[] = [];
-invoices.push(invOne)
-invoices.push(invTwo);
-// invoices.push({ name: 'shaun' });
-
-console.log(invoices);
-
-
+//const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 console.log(form.children);
 
