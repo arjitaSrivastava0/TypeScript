302b486481eddfb140e0f649d8f77a60a238c28a
arjitaSrivastava0 <arjita.sri.28@gmail.com>
2021-05-13 16:44:24 +0530

    more functionality in  generics

--- public/app.js
+++ public/app.js
@@ -49,3 +49,85 @@ const docFour = {
     data: ['bread', 'milk']
 };
 console.log(docThree, docFour);
+//some new additions 
+// const promise: Promise<any> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done! ');
+//   }, 2000);
+// });
+// promise.then(data => {
+// });
+//functions & constraints with generics
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergedObj = merge({ name: 'Arjita', hobbies: ['Dance'] }, { age: 23 });
+console.log(mergedObj.hobbies);
+//doesn't matter what type of elements we're getting
+//but it does have length property
+function countAndDescription(element) {
+    let descriptionText = 'Got no value.';
+    if (element.length === 1) {
+        descriptionText = 'got 1 element';
+    }
+    else if (element.length > 1) {
+        descriptionText = 'got ' + element.length + ' elements';
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescription('sdgf jsdgf'));
+//Keyof constraints
+//first parameter should be an object and the second
+//parameter should be any kind of key in that object
+function extractAndConvert(obj, key) {
+    return 'Value: ' + obj[key];
+}
+//passing an object along with key value which that object has
+//if we're passing any kind of an object but we don't have that key
+//then we'll get an array
+const result = extractAndConvert({ name: 'Arjita' }, 'name');
+console.log(result); // Arjita
+//Generic classes
+//useful when may be we just don;t wanna store text, 
+//might also want to stores number in different datastorage
+//can use any primitive value we want 
+class DataStorage {
+    constructor() {
+        //this data is of type T
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        //if check if we don't find that item
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data]; //everything 
+    }
+}
+//storing text in DataStorage
+const textstorage = new DataStorage();
+textstorage.addItem('Arjita');
+textstorage.addItem('gdfhg');
+textstorage.removeItem('gdfhg');
+console.log(textstorage);
+//storing number in DataStorage
+const numberStorage = new DataStorage();
+numberStorage.addItem(23);
+numberStorage.addItem(73);
+console.log(numberStorage);
+//partial turns courseGoal into a type
+//which is also an object type where all the properties are optional
+function createCourseGoal(title, description, date) {
+    let courseGoal = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal;
+}
+const names = ['Arjita', 'Srivastava'];
--- src/app.ts
+++ src/app.ts
@@ -71,3 +71,129 @@ const docFour: Resource<string[]> = {
 };
 
 console.log(docThree, docFour);
+
+
+//some new additions 
+
+// const promise: Promise<any> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done! ');
+//   }, 2000);
+// });
+
+// promise.then(data => {
+  
+// });
+
+//functions & constraints with generics
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({name: 'Arjita', hobbies: ['Dance']},{ age: 23});
+console.log(mergedObj.hobbies);
+
+interface Lengthy {
+  length: number;
+}
+
+//doesn't matter what type of elements we're getting
+//but it does have length property
+function countAndDescription<T extends Lengthy>(element: T) {
+  let descriptionText = 'Got no value.';
+
+  if(element.length === 1) {
+    descriptionText = 'got 1 element';
+  } else if(element.length > 1) {
+    descriptionText = 'got '+element.length+' elements';
+  }
+
+  return [element, descriptionText];
+}
+
+console.log(countAndDescription('sdgf jsdgf'));
+
+
+//Keyof constraints
+//first parameter should be an object and the second
+//parameter should be any kind of key in that object
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return 'Value: '+obj[key];
+}
+
+//passing an object along with key value which that object has
+//if we're passing any kind of an object but we don't have that key
+//then we'll get an array
+const result = extractAndConvert({name: 'Arjita'}, 'name');
+console.log(result);// Arjita
+
+//Generic classes
+//useful when may be we just don;t wanna store text, 
+//might also want to stores number in different datastorage
+
+//can use any primitive value we want 
+class DataStorage<T extends string | number | boolean> {
+  //this data is of type T
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    //if check if we don't find that item
+    if(this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];//everything 
+  }
+}
+
+//storing text in DataStorage
+const textstorage = new DataStorage<string>();
+textstorage.addItem('Arjita');
+textstorage.addItem('gdfhg');
+textstorage.removeItem('gdfhg');
+console.log(textstorage);
+
+//storing number in DataStorage
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(23);
+numberStorage.addItem(73);
+console.log(numberStorage);
+
+////storing object in DataStorage
+// const objectStorage = new DataStorage<object>();
+// const maxObj = {name: 'something'};
+// objectStorage.addItem({name: 'something'});
+// objectStorage.addItem({name: 'anything'});
+// objectStorage.removeItem(maxObj);
+// console.log(objectStorage.getItems());
+
+//Generic Utility
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+//partial turns courseGoal into a type
+//which is also an object type where all the properties are optional
+
+function createCourseGoal(
+  title: string, 
+  description: 
+  string, 
+  date: Date): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Arjita', 'Srivastava'];
